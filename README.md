2d014722cec88c20deb318b51baf4e2ec376bfce
ateeq72 <ahmedateeq64@gmail.com>
2015-03-29 05:04:03 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#by ateeq72@xda
+
+This is a Python code for RPi which can be used to control 1,2,3.. different specific
+tasks just by using clicks of a button! i.e one press performs a task and double
+performs other and so on! what tasks it performs can be seen using the code itself! :D
+
+Thank You! :)
---
+++ runme_button_config_v2.py
@@ -0,0 +1,98 @@
+import RPi.GPIO as GPIO
+import time
+import os
+from time import sleep
+
+sw_in = 26
+LED = 18
+file = open('/home/pi/PiraCast/status','r+w')
+f = file.read()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sw_in,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(sw_in,GPIO.FALLING)
+GPIO.setup(LED,GPIO.OUT)
+GPIO.setwarnings(False)
+
+def flashLED(speed, time):
+        for x in range(0, time):
+                GPIO.output(LED, GPIO.LOW)
+                sleep(speed)
+                GPIO.output(LED, GPIO.HIGH)
+                sleep(speed)
+
+def triplePress():
+       	flashLED(0.5, 3)
+	print ("Pressed Thrice So! Switching to Piracast!")
+	os.system('echo "You pressed the button thrice!. So, Switching to Piracast" | festival --tts')
+	if f[0] == "1":
+           os.system('echo "suitable environment found!. So, switching to Piracast" | festival --tts')
+	   os.system("sh /home/pi/runme_piracast.sh")
+	elif f[0] == "0":
+	   #print('place holder')
+	   os.system('echo "Sorry no suitable environment found!. Changing the file and rebooting!" | festival --tts')
+	   os.system("echo 1 > /home/pi/PiraCast/status")
+	   os.system("sh /home/pi/PiraCast/switch_to_piracast")
+
+def doublePress():
+	flashLED(0.5, 2)
+        print ("Pressed Twice So! Kodi! Here We come! :D")
+	os.system('echo "You pressed the button twice!. So, KODI here we come!" | festival --tts')
+        if f[0] == "1":
+	   print("Changing file and rebooting")
+	   os.system('echo "Sorry! no suitable environment found!. Changing the file and rebooting" | festival --tts') 
+	   os.system("echo 0 > /home/pi/PiraCast/status")
+	   os.system("sh /home/pi/PiraCast/switch_to_normal")
+        elif f[0] == "0" :
+	   os.system('echo "suitable environment found!. So, Starting KODI!." | festival --tts')
+	   print("Suitable situation found ! so starting")
+	   os.system("kodi-standalone")
+
+def singlePress():
+	flashLED(0.5, 1)
+        print ("Pressed Once So! Home Automation!")
+	os.system('echo "You pressed the button once!. So, Home Automation!" | festival --tts')
+        if f[0] == "1":
+	  print("Changing file and rebooting")
+	  os.system('echo "Sorry! no suitable environment found!. Changing the file and rebooting" | festival --tts')
+          os.system("echo 0 > /home/pi/PiraCast/status")
+          os.system("sh /home/pi/PiraCast/switch_to_normal")
+        elif f[0] == "0" :
+	  print("Suitable situation found ! so starting")
+	  os.system('echo "Suitable environment found ! so starting" | festival --tts')
+	  os.system('echo "use this Address to control your electrical devices!" | festival --tts && hostname -I | festival --tts && echo " : 3 7 0 0 / interface" | festival --tts')
+	  os.system("sudo -u pi /home/pi/HomeAutomation/pi-node-relay/runme.sh")
+        
+
+
+while True:
+   if GPIO.event_detected(sw_in):
+      GPIO.remove_event_detect(sw_in)
+      now = time.time()
+      count = 1
+      GPIO.add_event_detect(sw_in,GPIO.RISING)
+      while time.time() < now + 1: # 1 second period
+         if GPIO.event_detected(sw_in):
+            count +=1
+            time.sleep(.25) # debounce time
+      #print count
+      #performing required task!
+      if count == 2:
+	singlePress()
+	GPIO.remove_event_detect(sw_in)
+        GPIO.add_event_detect(sw_in,GPIO.FALLING)
+	#break
+      elif count == 3:
+	doublePress()
+	GPIO.remove_event_detect(sw_in)
+	GPIO.add_event_detect(sw_in,GPIO.FALLING)
+	#break
+      elif count == 4:
+	triplePress()
+        GPIO.remove_event_detect(sw_in)
+        GPIO.add_event_detect(sw_in,GPIO.FALLING)
+	#break
+
+file.close()
+
+GPIO.cleanup()
+GPIO.setwarnings(False)
